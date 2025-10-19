3f5343865a21cbc370518290100d989d6340ba3b
Paigack <460852283@qq.com>
2025-10-19 17:04:50 +0800

    clippy2

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -3,10 +3,4 @@
 // Execute `rustlings hint clippy2` or use the `hint` watch subcommand for a
 // hint.
 
-fn main() {
-    let mut res = 42;
-    while let Some(x) = Some(12) {
-        res += x;
-    }
-    println!("{}", res);
-}
+fn main() {}
